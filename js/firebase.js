8a70cfa5f59eb548c19340256aa275692e5b59c9
lasjunior99 <lasjunior99@gmail.com>
2025-11-28 00:28:24 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,13 @@
-const firebaseConfig={}; firebase.initializeApp(firebaseConfig); const db=firebase.firestore();
+const firebaseConfig = {
+  
+  apiKey: "AIzaSyBoua0vL6VrKlRZ90gZY58F0-qo52-yTjc",
+  authDomain: "aval-mapa-estrategico-lade.firebaseapp.com",
+  projectId: "aval-mapa-estrategico-lade",
+  storageBucket: "aval-mapa-estrategico-lade.firebasestorage.app",
+  messagingSenderId: "832018451740",
+  appId: "1:832018451740:web:311d14eb071778feaffed9"
+};
+
+// Inicialização
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
